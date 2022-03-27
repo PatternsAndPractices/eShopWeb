54c6e5a6334f6506e26deb01f81acffce351fa13
Julio Robles <jucer74@hotmail.com>
2022-03-26 19:29:02 -0500

    first commit

---
+++ Code/ShopWeb/src/Web/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.eShopWeb.Web
+{
+    public static class Constants
+    {
+        public const string CURRENT_VERSION = "1.0.0";
+        public const string BASKET_COOKIENAME = "eShop";
+        public const int ITEMS_PER_PAGE = 10;
+        public const string DEFAULT_USERNAME = "Guest";
+        public const string BASKET_ID = "BasketId";
+        public const string DEFAULT_ENVIRONMENT = "Development";
+    }
+}
